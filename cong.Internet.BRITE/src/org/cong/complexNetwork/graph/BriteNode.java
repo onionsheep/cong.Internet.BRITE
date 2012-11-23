8f39011c1a4d5dc311bc89d7e5964fe07e335605
onion_sheep <onion_sheep@163.com>
2012-11-24 04:07:06 +0800

    last work before sleep

---
+++ cong.Internet.BRITE/src/org/cong/complexNetwork/graph/BriteNode.java
@@ -0,0 +1,11 @@
+package org.cong.complexNetwork.graph;
+
+public class BriteNode extends Node {
+
+	public BriteNode(String id) {
+		super(id);
+	}
+	public BriteNode(String id, Double weight) {
+		super(id, weight);
+	}
+}
