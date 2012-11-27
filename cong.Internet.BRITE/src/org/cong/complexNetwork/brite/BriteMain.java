6b280c6f12a2bb9ee5557936b5be19a6c6df40b1
onion_sheep <onion_sheep@163.com>
2012-11-27 22:44:04 +0800

    use Configuration.json, output gexf file

---
+++ cong.Internet.BRITE/src/org/cong/complexNetwork/brite/BriteMain.java
@@ -0,0 +1,64 @@
+package org.cong.complexNetwork.brite;
+
+import it.uniroma1.dis.wiserver.gexf4j.core.Gexf;
+import it.uniroma1.dis.wiserver.gexf4j.core.impl.StaxGraphWriter;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Set;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.cong.complexNetwork.graph.BriteNode;
+import org.cong.complexNetwork.model.BriteBA;
+import org.cong.complexNetwork.model.BritePlane;
+import org.cong.complexNetwork.model.BriteWaxman;
+
+import com.alibaba.fastjson.JSON;
+
+
+public class BriteMain {
+	
+	public static Logger logger = LogManager.getLogger(BriteMain.class);
+	
+	public static void main(String[] args) {
+		File file = new File("Configuration.json");
+		try {
+			String confStr = FileUtils.readFileToString(file);
+			//Configuration conf = Json.fromJson(Configuration.class, confStr);  //nutz的json解析
+			Configuration conf = JSON.parseObject(confStr, Configuration.class);
+			generate(conf);
+		} catch (IOException e) {
+			logger.error("配置文件读取错误，请检查。");
+			e.printStackTrace();
+		}
+	}
+
+	private static void generate(Configuration configuration){
+		BritePlane britePlane = new BritePlane(configuration.hs, configuration.ls);
+		britePlane.addRandomNodes(configuration.waxmanNodeCount);
+		logger.info("正在生成Waxman随机图...");
+		BriteWaxman.generateEdges(britePlane, configuration.waxmanAlpha, configuration.waxmanBeta);
+
+		logger.info("正在生成BA图...");
+		BriteBA.generateEdges(britePlane, configuration.baOneNodeEdge, configuration.baNodeCount);
+		logger.info("正在把模型转化为GEXF格式...");
+		
+		Gexf gexf = britePlane.getBriteGraph().toGexf();
+		logger.debug("正在输出为文件...");
+		StaxGraphWriter graphWriter = new StaxGraphWriter();
+		File f = new File(configuration.outputFileName);
+		Writer out;
+		try {
+			out = new FileWriter(f, false);
+			graphWriter.writeToStream(gexf, out, "UTF-8");
+			logger.info("文件已保存" + f.getAbsolutePath());
+		} catch (IOException e) {
+			logger.error("保存文件失败");
+			e.printStackTrace();
+		}
+	}
+}
---
+++ cong.Internet.BRITE/src/org/cong/complexNetwork/brite/Configuration.java
@@ -0,0 +1,78 @@
+package org.cong.complexNetwork.brite;
+
+public class Configuration {
+	public Integer	hs;
+	public Integer	ls;
+	public Integer	waxmanNodeCount;
+	public Double		waxmanAlpha;
+	public Double		waxmanBeta;
+	public Integer	baNodeCount;
+	public Integer	baOneNodeEdge;
+	public String		outputFileName;
+
+	public Integer getHs() {
+		return hs;
+	}
+
+	public void setHs(Integer hs) {
+		this.hs = hs;
+	}
+
+	public Integer getLs() {
+		return ls;
+	}
+
+	public void setLs(Integer ls) {
+		this.ls = ls;
+	}
+
+	public Integer getWaxmanNodeCount() {
+		return waxmanNodeCount;
+	}
+
+	public void setWaxmanNodeCount(Integer waxmanNodeCount) {
+		this.waxmanNodeCount = waxmanNodeCount;
+	}
+
+	public Double getWaxmanAlpha() {
+		return waxmanAlpha;
+	}
+
+	public void setWaxmanAlpha(Double waxmanAlpha) {
+		this.waxmanAlpha = waxmanAlpha;
+	}
+
+	public Double getWaxmanBeta() {
+		return waxmanBeta;
+	}
+
+	public void setWaxmanBeta(Double waxmanBeta) {
+		this.waxmanBeta = waxmanBeta;
+	}
+
+	public Integer getBaNodeCount() {
+		return baNodeCount;
+	}
+
+	public void setBaNodeCount(Integer baNodeCount) {
+		this.baNodeCount = baNodeCount;
+	}
+
+	public Integer getBaOneNodeEdge() {
+		return baOneNodeEdge;
+	}
+
+	public void setBaOneNodeEdge(Integer baOneNodeEdge) {
+		this.baOneNodeEdge = baOneNodeEdge;
+	}
+
+	public String getOutputFileName() {
+		return outputFileName;
+	}
+
+	public void setOutputFileName(String outputFileName) {
+		this.outputFileName = outputFileName;
+	}
+
+	
+}
